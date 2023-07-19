cf33c78524f13196ae123aa63dbff19633b3d274
felipegarciamf <silas.garcia@mfconsulting.com.br>
2023-07-19 18:50:55 -0300

    retirando configs não utilizadas

--- main.tf
+++ main.tf
@@ -1,29 +1,7 @@
-
-
 provider "aws" {
-
-
   region = "us-east-2" # substitua pela região desejada
-
-  # Outras configurações opcionais, como perfil ou credenciais específicas:
-  #profile = "default"
-  #access_key                  = "AKIASWNGAKDJGBMSMM5I"
-  #secret_access_key           = "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"
 }
 
-// LOCALSTACK
-/*
-provider "aws" {
-  access_key                  = "AKIASWNGAKDJGBMSMM5I"
-  secret_access_key           = "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"
-  region                      = "us-east-1"
-  s3_force_path_style         = true
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-  endpoint                    = "http://localhost:4566"  # Endpoint do LocalStack
-}
-*/
 
 # Ler o conteúdo dos arquivos JSON individuais
 locals {
